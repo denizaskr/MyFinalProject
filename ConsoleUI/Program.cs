01b2db19d2646ff7f1999859c719aeea4efd42e2
denizaskar05 <denizaskar05@gmail.com>
2023-08-26 23:55:25 +0300

    Fourth Commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProducTest();
+            ProductTest();
             //CategoryTest();
         }
 
@@ -23,13 +23,22 @@ namespace ConsoleUI
             }
         }
 
-        private static void ProducTest()
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success==true)
             {
-                Console.WriteLine(product.ProductName+ "/"+ product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
         }
     }
 }
