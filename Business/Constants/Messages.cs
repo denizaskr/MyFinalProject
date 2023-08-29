7dee7f752c3c21c5a488cb62be988981720e65a6
denizaskar05 <denizaskar05@gmail.com>
2023-08-29 15:46:48 +0300

    Sixth Commit

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,6 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem Bakımda";
         public static string ProductsListed="Ürünler Listelendi";
+        
     }
 }
